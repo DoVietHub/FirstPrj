67d4ad5fcd73acc2da71688e5fa78f053fb665d2
DoVietHub <dovanviet03777@gmail.com>
2024-08-16 14:16:36 +0700

    message

---
+++ src/Wecome.java
@@ -0,0 +1,6 @@
+public class Wecome {
+    public static void main(String[] args) {
+        System.out.println("Hello Do Van Viet!");
+        System.out.println("welcome to the DHHTTT19A!");
+    }
+}
